cefc309216d8d5d7df428480c1078857101f2dc2
adujoseph <iamjosephadu@gmail.com>
2019-04-06 14:32:28 +0100

    fix npm cache related bug

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -7,7 +7,7 @@ import {
 } from 'react-native';
 
 import Logo from '../components/Logo';
-import Form from '../components/Form';
+import Form from '../components/Forms';
 
 import {Actions} from 'react-native-router-flux';
 
